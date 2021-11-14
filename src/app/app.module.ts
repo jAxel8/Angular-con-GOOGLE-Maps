32ae39ee0e93864104fac8c294b1ae27ed58af6e
jAxel8 <jaxelsaucedo@gmail.com>
2021-11-14 06:17:24 -0600

    angular-maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from "@agm/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot(
+      {apiKey:'AIzaSyAsv6YyCVteGu4LvpreebfdT8LWYltzUf4',
+      libraries: ['places']
+    }
+    ),
+    NgbPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
